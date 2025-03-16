3574853666b96215218f26d203e2ae5b791a986c
testing17741 <154995858+testing17741@users.noreply.github.com>
2025-03-16 20:25:42 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,5 @@
+# config.py
+API_ID = 21770451                       # Your API ID
+API_HASH = "842e6188ff744c8f6c2958fadc785f42"  # Your API HASH
+PHONE = "+918097266112"                 # Your phone number in +91XXXXXXXXXX format
+GROUP_ID = -1002221484902               # Your private group ID
